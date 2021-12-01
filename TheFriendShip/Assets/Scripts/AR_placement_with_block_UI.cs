e7dcd95cf73b7153a9e5760b9bffc412fe48649a
TheOneTrueNoodle <72693746+TheOneTrueNoodle@users.noreply.github.com>
2021-12-01 18:39:38 +0000

    Added UI and ability to choose placable objects

---
+++ TheFriendShip/Assets/Scripts/AR_placement_with_block_UI.cs
@@ -0,0 +1,56 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.XR.ARFoundation;
+
+[RequireComponent(typeof(ARRaycastManager))]
+public class AR_placement_with_block_UI : MonoBehaviour
+{
+    public GameObject placedPrefab;
+
+    [SerializeField]
+    private GameObject uiPanel;
+
+    [SerializeField]
+    private GameObject ToggleButton;
+
+    private ARRaycastManager arRaycastManager;
+
+    private static List<ARRaycastHit> hits = new List<ARRaycastHit>();
+
+    private void Awake()
+    {
+        arRaycastManager = GetComponent<ARRaycastManager>();
+    }
+
+    public void Toggle()
+    {
+        uiPanel.SetActive(!uiPanel.activeSelf);
+        var toggleButtonText = ToggleButton.gameObject.GetComponentInChildren<TextMeshProUGUI>();
+        toggleButtonText.text = uiPanel.activeSelf ? "UI OFF" : "UI ON";
+    }
+
+    private void Update()
+    {
+        if(Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+
+            if(touch.phase == TouchPhase.Began)
+            {
+                var touchPosition = touch.position;
+
+                bool isOverUi = touchPosition.IsPointOverUIObject();
+
+                if(!isOverUi && arRaycastManager.Raycast(touchPosition, hits, UnityEngine.XR.ARSubsystems.TrackableType.PlaneWithinPolygon))
+                {
+                    var hitPose = hits[0].pose;
+                    Instantiate(placedPrefab, hitPose.position, hitPose.rotation);
+                }
+            }
+        }
+    }
+}
---
+++ TheFriendShip/Assets/Scripts/ObjectSelect.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.XR.ARFoundation;
+
+public class ObjectSelect : MonoBehaviour
+{
+    [SerializeField] private GameObject RedPrefab;
+    [SerializeField] private GameObject YellowPrefab;
+    [SerializeField] private GameObject BluePrefab;
+    [SerializeField] private GameObject RedButton;
+    [SerializeField] private GameObject YellowButton;
+    [SerializeField] private GameObject BlueButton;
+    private AR_placement_with_block_UI ARPlacement;
+
+    private void Awake()
+    {
+        ARPlacement = FindObjectOfType<ARSessionOrigin>().GetComponent<AR_placement_with_block_UI>();
+    }
+
+    public void RedButtonToggle()
+    {
+        ARPlacement.placedPrefab = RedPrefab;
+    }
+    public void YellowButtonToggle()
+    {
+        ARPlacement.placedPrefab = YellowPrefab;
+    }
+    public void BlueButtonToggle()
+    {
+        ARPlacement.placedPrefab = BluePrefab;
+    }
+}
